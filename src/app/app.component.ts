dc346770651392ecd5533f16891dc72b39a26cda
DireccionInformaticaFhycs <direccioninformatica@fhycs.unju.edu.ar>
2024-07-03 13:25:48 -0300

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
@@ -9,7 +8,7 @@ import { FooterComponent } from "./components/footer/footer.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, HeaderComponent, HomeComponent, FooterComponent]
+    imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'jornadascongresos';
--- src/app/components/eventos2022/eventos2022.component.ts
+++ src/app/components/eventos2022/eventos2022.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-eventos2022',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './eventos2022.component.html',
   styleUrl: './eventos2022.component.css'
 })
